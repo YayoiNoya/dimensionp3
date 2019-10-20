1919a5d3960834fc15f78974a151b134a5e49d0d
NagaYaorochi <contact.nao.7@gmail.com>
2019-10-20 19:52:48 +0900

    dimensionp3 first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "DimensionP3 - ゲーム企画合宿 | ToC",
+    author: "Naga(ToC)",
+    description: "ゲーム合宿企画のキャンペーンページ。シングルページアプリケーション（SPA）の実験用サイト"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,20 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>DimensionP3</h1>
+                <p>ボードゲームだって、TRPGだって、遊び倒せ。<br />
+                さあ、オフラインだけの遊びを。</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('schedule')}}>Schedule</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('history')}}>History</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+            <ul className="entrybtn">
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Entry</a></li>
             </ul>
         </nav>
     </header>
